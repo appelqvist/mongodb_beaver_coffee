13e7d984bdbf3a6cdb7cbbf6f8dfea4c14a514a2
Andréas Appelqvist <andreasappelqvist@Andreass-MacBook-Pro.local>
2017-05-11 11:05:26 +0200

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by andreasappelqvist on 2017-05-11.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hej grabbar, nu kör vi!!!!!!");
+    }
+}
