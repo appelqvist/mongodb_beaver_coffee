47f0a14ca7991268de16bf0ad56858ad3d02f822
Andréas Appelqvist <andreasappelqvist@Andreass-MacBook-Pro.local>
2017-05-16 10:05:34 +0200

    Client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,5 @@
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.gridfs.CLI;
-
 import javax.swing.*;
 
 /**
@@ -40,7 +38,45 @@ public class Client {
     public void start(){
         while (true){
             int choice;
-            choice = Integer.parseInt(JOptionPane.showInputDialog("Pick one of the alternative: "));
+            choice = Integer.parseInt(JOptionPane.showInputDialog("Pick one of the alternative: \n" +
+                    "1: Employee"));
+            switch (choice){
+                case 1 :
+                    employeePick();
+                    break;
+                default:
+                    System.exit(0);
+                    break;
+            }
+        }
+    }
+
+
+    private void employeePick(){
+        while (true){
+            int choice;
+            choice = Integer.parseInt(JOptionPane.showInputDialog("1: Add employee\n"+
+                                                                "0: Exit employee"));
+            switch (choice){
+                case 1:
+                    //Method??
+                    String name = JOptionPane.showInputDialog("Choose name:");
+                    String ssn = JOptionPane.showInputDialog("Choose SSN:");
+                    String salary = JOptionPane.showInputDialog("Choose salary:");
+                    int discount = Integer.parseInt(JOptionPane.showInputDialog("Choose discount:"));
+                    String position = JOptionPane.showInputDialog("Choose position:");
+                    String startDate = JOptionPane.showInputDialog("Choose start date:\n" +
+                            "Format: YYYY-MM-DD");
+                    String endDate = JOptionPane.showInputDialog("Choose end date:\n" +
+                            "Format: YYYY-MM-DD");
+                    String workingTime = JOptionPane.showInputDialog("Choose working time (%):");
+                    String storeID = JOptionPane.showInputDialog("Select id of the store:\n(hämta alla store och visa här)");
+                    employeeCollection.addEmployee(name,ssn,salary,discount,position,startDate,endDate,workingTime,storeID);
+                    break;
+
+                case 2:
+                    break;
+            }
         }
     }
 }
