7fae0f8fe93b8425cac11406deda958e9ed73109
anders.frid85@gmail.com <anders.frid85@gmail.com>
2017-05-22 15:20:55 +0200

    Added stock to store

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,9 +1,13 @@
+import com.mongodb.DBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -35,7 +39,29 @@ public class Store {
     }
 
     public void addToStock(String storeID, String name, int unit){
-
+        Document doc = new Document("id", "1").append("name", "espresso")
+                .append("units", 10000);
+        Document doc1 = new Document("name", "french roast")
+                .append("units", 10000);
+        Document doc2 = new Document("name", "light roast")
+                .append("units", 10000);
+        Document doc3 = new Document("name", "coca mix")
+                .append("units", 10000);
+        Document doc4 = new Document("name", "skim milk")
+                .append("units", 10000);
+        Document doc5 = new Document("name", "soy milk")
+                .append("units", 10000);
+        Document doc6 = new Document("name", "whole milk")
+                .append("units", 10000);
+        Document doc7 = new Document("name", "2% milk")
+                .append("units", 10000);
+        Document doc8 = new Document("name", "vanilla syrup")
+                .append("units", 10000);
+        Document doc9 = new Document("name", "caramel syrup")
+                .append("units", 10000);
+        Document doc10 = new Document("name", "irish cream syrup")
+                .append("units", 10000);
     }
+
 
 }
